5a44fb88572f2d140447881827aa09ce2af70586
Azeem Virji <virjia4@mcmaster.ca>
2023-01-18 23:38:53 -0500

    Add project files.

---
+++ ExpenseTracker/Migrations/20230113025002_FixCategoryId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCategoryId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Catergory",
+                table: "Categories",
+                newName: "CatergoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CatergoryId",
+                table: "Categories",
+                newName: "Catergory");
+        }
+    }
+}
---
+++ ExpenseTracker/Migrations/20230113030343_Fixed Spelling.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedSpelling : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CatergoryId",
+                table: "Categories",
+                newName: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Categories",
+                newName: "CatergoryId");
+        }
+    }
+}
